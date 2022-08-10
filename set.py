5f3504b182e6994b417745c768c20febdb0ac651
Mr BIV <47798805+Naptwen@users.noreply.github.com>
2022-08-10 23:37:51 +0900

    Create set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+setup(
+  name = 'open_pyAI',
+  version = '2.2.4',
+  description='open_pyAI pip installation',
+  url='https://github.com/Naptwen/Open_pyAI.git',
+  autho='Useop Gim',
+  license='GNU AFFERO (c) 2022 Useop Gim',
+  packages=['open_pyAI'],
+  zip_safe=False,
+  install_requires=[
+    'numpy==1.23.0',
+    'colorama==0.4.5',
+    'sympy==1.10.1'
+  ]
+)
